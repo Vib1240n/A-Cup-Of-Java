f8e0612907fe3ddede1ddb1e5d13bd30ab360358
Vibhore Sagar <vibhorsgr@gmail.com>
2022-04-29 19:21:40 -0700

    user

--- js/passwordHash.js
+++ js/passwordHash.js
@@ -4,11 +4,11 @@ const bcrypt = require("bcrypt");
 const User = require("../model/user");
 const salt = await bcrypt.genSalt(10);
 
-router.post("/SignUpPage.html", async (req, res) => {
+router.post("/signup", async (req, res) => {
   const body = req.body;
   const user = new User(body);
   user.password = await bcrypt.hash(user.password, salt);
   user.save().then((doc) => res.status(201).send(doc));
 });
 
-module.exports = router;
+module.exports = hash;
