10e93d70dfec24d74c88e7adcac6a021af3a3904
Vibhore Sagar <vibhorsgr@gmail.com>
2022-04-29 18:23:33 -0700

    db

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+var URL = process.env.url;
 
 //Create route
 const indexRouter = require("./routes/index");
@@ -9,8 +12,8 @@ app.use("/", indexRouter);
 
 //static files
 app.use(express.static("Assets"));
-app.use("/css", express.static(__dirname + "Assets/css"))
-app.use("/images", express.static(__dirname+ "Assets/images"))
+app.use("/css", express.static(__dirname + "Assets/css"));
+app.use("/images", express.static(__dirname + "Assets/images"));
 
 //templates
 app.set("views", __dirname + "/views");
@@ -19,13 +22,10 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 
 mongoose
-  .connect(
-    "mongodb+srv://admin:<password>@cluster0.zfduz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("connected to database"))
   .catch(console.error);
 
