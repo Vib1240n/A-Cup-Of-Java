4b15cb37d1585036e7e5d0fc27f82c2930c4284c
Vibhore Sagar <vibhorsgr@gmail.com>
2022-04-30 22:35:43 -0700

    AJC-29 Modified database and fixed ejs views

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const dotenv = require("dotenv").config();
 const app = express();
 const mongoose = require("mongoose");
 
