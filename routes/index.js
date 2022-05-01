6adf6e6947773e0142bc39a1c4732ad735fc8f46
Thomas Nguyen <thomasduynguyenviet@gmail.com>
2022-04-30 21:59:44 -0700

    AJC-47 Linked Existing views of website together

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const user = require("../model/user");
+const async = require("hbs/lib/async");
+const bcrypt = require("bcrypt");
+const salt = bcrypt.genSalt(10);
+
+var bodyParser = require("body-parser");
+var jsonparser = bodyParser.json();
+var urlencodedparser = bodyParser.urlencoded({ extended: false });
+const user = require("../model/User");
 
 router.get("/", async (req, res) => {
   res.render("home");
+  //200
 });
 
 router.get("/home", async (req, res) => {
-    res.render("home")
-})
+  res.render("home");
+});
+
 router.get("/login", async (req, res) => {
+  res.render("login");
+});
+
+router.post("/login", async (req, res) => {
   res.render("login");
 });
 
@@ -17,5 +30,19 @@ router.get("/signup", async (req, res) => {
   res.render("signup");
 });
 
+router.get("/barbers", async (req, res) => {
+  res.render("barbers");
+});
 
+router.get("/appointments", async (req, res) => {
+  res.render("appointments");
+});
+
+router.get("/services", async (req, res) => {
+  res.render("services");
+});
+
+router.get("/about", async (req, res) => {
+  res.render("about");
+});
 module.exports = router;
