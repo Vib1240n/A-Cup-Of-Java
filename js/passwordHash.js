3d0273781f162d43adabf8747736107468303349
Vibhore Sagar <vibhorsgr@gmail.com>
2022-05-12 17:32:22 -0700

    Adding user validation using Joi - test

--- js/passwordHash.js
+++ js/passwordHash.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const salt = bcrypt.genSalt(10);
 
 async function hashing(userPassword){
-  password = bcrypt.hash(userPassword, salt);
+  password = bcrypt.hash(userPassword, parseInt(salt));
   return password;
 }
 module.exports = hashing;
--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Joi = require("joi")
 
 var user = Schema({
   name: {
@@ -23,4 +24,16 @@ var user = Schema({
   },
 });
 
-module.exports = mongoose.model("user", user);
+function validation(user){
+  const schema = {
+    name: Joi.string().min(4).max(255).required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().min(8).max(2048).required(),
+  };
+  return Joi.validate(user, schema);
+}
+
+exports.user = user;
+exports.validation = validation;
+// module.exports = mongoose.model("user", user);
+// module.exports = validation;
