fcc8a3198bb8b1497bb2fc565b08071a6f458280
Vibhore Sagar <vibhorsgr@gmail.com>
2022-09-03 19:24:50 -0700

    Server.js changes

--- Server/server.js
+++ Server/server.js
@@ -4,18 +4,18 @@ const app = express();
 var bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./Private/.env" });
+dotenv.config({ path:"./Private/.env"});
 
 //Create route
 const indexRouter = require("../Server/routes/index");
 app.use("/", indexRouter);
 
-//static files
+//Static files
 app.use(express.static("/Public/Assets"));
 app.use("/css", express.static(__dirname + "./Public/Assets/css"));
 app.use("/images", express.static(__dirname + "./Public/Assets/images"));
 
-//templates
+//Templates
 app.set("views", __dirname + "./Public/views");
 app.set("view engine", "ejs");
 app.set("layout", "layouts/layout");
@@ -24,6 +24,8 @@ app.use(expressLayouts);
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
+
+//Database connection process
 mongoose
   .connect(process.env.ADMIN_URI, {
     useNewUrlParser: true,
@@ -32,4 +34,5 @@ mongoose
   .then(() => console.log("connected to database"))
   .catch(console.error);
 
+//Port to listen to
 app.listen(process.env.PORT);
