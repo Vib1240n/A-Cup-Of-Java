7feeedc8abfc844de6d88ebb5deee59d01cf4973
Vibhore Sagar <vibhorsgr@gmail.com>
2022-09-06 14:18:38 -0700

    AJC-72 Removing duplicate files from file tree and cleanup

--- Server/Authentication/passwordHash 2.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcrypt");
-const salt = bcrypt.genSalt(10);
-
-async function hashing(userPassword){
-  password = bcrypt.hash(userPassword, parseInt(salt));
-  return password;
-}
-module.exports = hashing;
