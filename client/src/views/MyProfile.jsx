21605fad7e4497677fde78b0a4cb1acc19e2ea88
arsmith97 <alex@s3v.us>
2022-10-04 22:23:52 -0700

    AJC-74 Implement Sign in API

--- client/src/views/MyProfile.jsx
+++ client/src/views/MyProfile.jsx
@@ -1,34 +1,40 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
-import "../asset/css/myProfile.css"
+import axios from "axios";
+import "../asset/css/myProfile.css";
+import { Component } from "react";
 
-export default function MyProfile() {
-  const [userData, setUserData] = useState('');
-
-  useEffect(() => {
-    fetchUserData();
-  }, []);
-
-  const fetchUserData = () => {
-    axios.get('http://localhost:5500/MyProfile').then((res) =>{
-      console.log(res);
-      setUserData(res.data);
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+export class MyProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      firstName: "",
+      lastName: "",
+    };
   }
-  
+  componentDidMount() {
+    axios.get("http://localhost:5500/api/MyProfile").then((res) => {
+      this.setState({
+        email: res.data.email,
+        firstName: res.data.firstName,
+        lastName: res.data.lastName,
+      });
+    });
+  }
 
-  return (
-    <div className="MyProfile">
-    <h1 className="heading">Your Profile Information</h1>
-    <div className="form" action="#">
-      <div className="firstName">First Name: {userData.firstName}</div>
-      <div className="firstName">Last Name: {userData.lastName}</div>
-      <div className="firstName">Email: {userData.username}</div>
-      <button type="submit">Sign Out</button>
-    </div>
-  </div>
-  ); 
+  render() {
+    return (
+      <div className="MyProfile">
+        <h1 className="heading">Your Profile Information</h1>
+        <div className="form" action="#">
+          <div className="firstName">First Name: {this.firstName}</div>
+          <div className="firstName">Last Name: {this.lastName}</div>
+          <div className="firstName">Email: {this.email}</div>
+          <button type="submit">Sign Out</button>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default MyProfile;
