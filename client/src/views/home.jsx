4aa3462ef86fda6e04d7928182e559bc959a1400
vib1240n <vibhorsgr@gmail.com>
2022-10-09 14:15:50 -0700

    Changes to navbar

--- client/src/views/home.jsx
+++ client/src/views/home.jsx
@@ -1,48 +1,49 @@
 import React from "react";
 import "../asset/css/style.css";
+import { useNavigate } from "react-router-dom";
 import Appointment from "./appointment";
-import InstagramIcon from "../asset/images/Instagram-Icon.png"
-import Facebookicon from "../asset/images/facebookiconcolor1.png"
-
+import InstagramIcon from "../asset/images/Instagram-Icon.png";
+import Facebookicon from "../asset/images/facebookiconcolor1.png";
 
 function Home() {
-
+  let navigate = useNavigate();
+  const handleChange = () => {
+    navigate("/appointment");
+  };
   return (
-    <div class="home-background">
-        <div class="home-title">
-          Welcome To Ace's Barbershop
+    <>
+      <div className="home-background">
+        <div className="home-title">Welcome To Ace's Barbershop</div>
+        <div className="home-secondtitle">
+          We provided all in one services including Haircute & Grooming
         </div>
-        <div class="home-secondtitle">
-              We provided all in one services including Haircute & Grooming
-        </div>
-        <br/>
-        {/* <button class= "home-button" onClick= "./Appointment"> Book Now </button> */}
-        <button class= "home-button" onClick = {Appointment}>
-              <a role href="/Appointment" class= "home-booknow"> Book Now </a> 
+        <br />
+        <button className="home-button" onClick={handleChange}>
+          <a>Book Now</a>
         </button>
 
-        <div class = "home-middleblock">
-              <div class="home-line-1"></div>
-              <h1 id = "home-font"> Find Us On </h1>
-              <div class = "home-twoicon">
-                <a href="https://www.instagram.com">
-                  <img src= {InstagramIcon} alt="Instagram Icon" id ="home-igicon"/>
-                </a>
-                <a href="https://www.facebook.com">
-                  <img src= {Facebookicon} alt="Instagram Icon" id ="home-fbicon"/>
-                </a>
-              </div>
-              <div class="home-line-2"/>
-                  <div class = "home-line22" id = "home-font">
-                    <p id = "home-contact"> Contact Us: 123 456789</p>
-                    <p id = "home-address">  1049 Jefferson Blvd, West Sacramento, CA 95691 </p>
-                    
-                  </div>
-                 
+        <div className="home-middleblock">
+          <div className="home-line-1"></div>
+          <h1 id="home-font"> Find Us On </h1>
+          <div className="home-twoicon">
+            <a href="https://www.instagram.com">
+              <img src={InstagramIcon} alt="Instagram Icon" id="home-igicon" />
+            </a>
+            <a href="https://www.facebook.com">
+              <img src={Facebookicon} alt="Instagram Icon" id="home-fbicon" />
+            </a>
+          </div>
+          <div className="home-line-2" />
+          <div className="home-line22" id="home-font">
+            <p id="home-contact"> Contact Us: 123 456789</p>
+            <p id="home-address">
+              1049 Jefferson Blvd, West Sacramento, CA 95691
+            </p>
+          </div>
         </div>
-       
-    </div>
-  )
+      </div>
+    </>
+  );
 }
 
 export default Home;
