63ab47163fef85df43b537016fc58d511d38f669
vib1240n <vibhorsgr@gmail.com>
2022-10-13 19:29:42 -0700

    Changes

--- client/src/Components/navbar_mui.js
+++ client/src/Components/navbar_mui.js
@@ -13,6 +13,7 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import Sidebar from "./sidebar_mui";
 import { MultiSelectUnstyled } from "@mui/base";
+import { Grid } from "@mui/material";
 
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
@@ -37,52 +38,160 @@ const ResponsiveAppBar = () => {
   };
 
   return (
-    <>
-    <Sidebar/>
-      <AppBar
-        sx={{
-          // background: "linear-gradient(45deg, #909090 30%, #151515 70%)",
-          background: "transparent",
-          boxShadow: "10",
-          backdropFilter: "blur(30px)",
-          height: "75px",
-          top: 0,
-          left: 0,
-          position: "fixed",
-          width: "100%",
-          display: "block",
-        }}
-      >
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
-            <Typography
-              variant="h6"
-              noWrap
+    <AppBar
+      sx={{
+        background: "linear-gradient(45deg, #909090 30%, #151515 70%)",
+        background: "transparent",
+        boxShadow: "20",
+        backdropFilter: "blur(40px)",
+        height: "75px",
+        position: "fixed",
+        width: "100vw",
+        display: "block",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar variant="regular">
+          <Grid
+            container
+            direction="row"
+            display={{ xs: "block", sm: "flex" }}
+            justifyContent={{ md: "flex-start", xs: "flex-end" }}
+            alignItems={{ md: "baseline", xs: "flex-end" }}
+            sx={{
+              width: { xs: "25%", md: "35%", lg: "20%" },
+            }}
+          >
+            {/** Shows when screen is maximum  */}
+            <Button
+              variant="h5"
               component="a"
-              href="/"
+              href="/home"
               sx={{
-                mr: 10,
-                display: { xs: "none", md: "flex" },
+                mr: 15,
+                display: { xs: "none", md: "flex", lg: "flex" },
+                color: "white",
                 fontFamily: "monospace",
-                fontWeight: 700,
+                fontWeight: 800,
                 letterSpacing: ".3rem",
                 position: "sticky",
+                width: "100vw",
                 top: "0px",
-                color: "inherit",
-                right: "750px",
-                textDecoration: "none",
-                zIndex: "5",
+                color: "black",
                 "&:hover": {
-                  color: "black",
-                  padding: "0.5rem",
+                  color: "white",
+                  backgroundColor: "black",
+                  width: "100vh",
                 },
               }}
             >
               Ace's BarberShop
-            </Typography>
+            </Button>
+            {/** Shows when screen is small or extra small  */}
+            <Button
+              variant="h5"
+              component="a"
+              href="/home"
+              sx={{
+                display: { xs: "flex", sm: "flex", md: "none" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".1rem",
+                color: "black",
+                textDecoration: "none",
+                "&:hover": {
+                  color: "white",
+                  backgroundColor: "black",
+                },
+              }}
+            >
+              Ace's BarberShop
+            </Button>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justifyContent={{ md: "flex-end", xs: "none" }}
+            display={{ md: "flex", xs: "none" }}
+            width={{ md: "65%", xs: "none" }}
+            border={"1px solid black"}
+            alignItems="baseline"
+            sx={{}}
+          >
+            <Button
+              display={{ md: "flex", xs: "none" }}
+              variant="h7"
+              component="a"
+              href="/Services"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".1rem",
+                color: "White",
+                textDecoration: "none",
+                "&:hover": {
+                  color: "black !important",
+                  backgroundColor: "White",
+                },
+              }}
+            >
+              Services
+            </Button>
+            <Button
+              display={{ md: "flex", xs: "none" }}
+              variant="h7"
+              component="a"
+              href="/about"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".1rem",
+                color: "White",
+                textDecoration: "none",
+                "&:hover": {
+                  color: "black !important",
+                  backgroundColor: "White",
+                },
+              }}
+            >
+              About Us
+            </Button>
+            <Button
+              display={{ md: "flex", xs: "none" }}
+              variant="h7"
+              component="a"
+              href="/appointment"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".1rem",
+                color: "White",
+                textDecoration: "none",
+                "&:hover": {
+                  color: "black !important",
+                  backgroundColor: "White",
+                },
+              }}
+            >
+              Appointment
+            </Button>
+          </Grid>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default ResponsiveAppBar;
 
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+/*{
+
+  <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}>
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorElNav}
@@ -101,98 +210,9 @@ const ResponsiveAppBar = () => {
                   display: { xs: "block", md: "none" },
                 }}
               ></Menu>
-            </Box>
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/home"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".1rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              Ace's BarberShop
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              <Button>
-                <Typography
-                  variant="h7"
-                  noWrap
-                  component="a"
-                  href="/Services"
-                  sx={{
-                    mr: 2,
-                    display: { xs: "none", md: "flex" },
-                    fontFamily: "monospace",
-                    fontWeight: 700,
-                    letterSpacing: ".1rem",
-                    color: "White",
-                    textDecoration: "none",
-                    "&:hover": {
-                      color: "black !important",
-                      backgroundColor: "White",
-                    },
-                  }}
-                >
-                  Services
-                </Typography>
-              </Button>
-              <Button>
-                <Typography
-                  variant="h7"
-                  noWrap
-                  component="a"
-                  href="/about"
-                  sx={{
-                    mr: 2,
-                    display: { xs: "none", md: "flex" },
-                    fontFamily: "monospace",
-                    fontWeight: 700,
-                    letterSpacing: ".1rem",
-                    color: "White",
-                    textDecoration: "none",
-                    "&:hover": {
-                      color: "black !important",
-                      backgroundColor: "White",
-                    },
-                  }}
-                >
-                  About Us
-                </Typography>
-              </Button>
-              <Button>
-                <Typography
-                  variant="h7"
-                  noWrap
-                  component="a"
-                  href="/appointment"
-                  sx={{
-                    mr: 2,
-                    display: { xs: "none", md: "flex" },
-                    fontFamily: "monospace",
-                    fontWeight: 700,
-                    letterSpacing: ".1rem",
-                    color: "White",
-                    textDecoration: "none",
-                    "&:hover": {
-                      color: "black !important",
-                      backgroundColor: "White",
-                    },
-                  }}
-                >
-                  Pricing
-                </Typography>
-              </Button>
-            </Box>
+            </Box> 
 
-            <Box sx={{ flexGrow: 0 }}>
+             <Box sx={{ flexGrow: 0 }}>
               <Tooltip title="Open settings">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                   <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
@@ -220,11 +240,118 @@ const ResponsiveAppBar = () => {
                   </MenuItem>
                 ))}
               </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </>
-  );
-};
-export default ResponsiveAppBar;
+            </Box> 
+
+}*/
+
+/* Buttons for the navbar
+<Button
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 15,
+              border: "1px solid black",
+              display: { xs: "none", md: "flex", lg: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              position: "sticky",
+              top: "0px",
+              color: "black",
+              // right: "750px",
+              // zIndex: "5",
+              "&:hover": {
+                color: "black",
+                padding: "0.5rem",
+              },
+            }}
+          >
+            Ace's BarberShop
+          </Button>
+          <Button
+            variant="h5"
+            noWrap
+            component="a"
+            href="/home"
+            sx={{
+              mr: 2,
+              border: "1px solid black",
+              display: { xs: "flex", sm: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".1rem",
+              color: "black",
+              textDecoration: "none",
+            }}
+          >
+            Ace's BarberShop
+          </Button>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Button
+              variant="h7"
+              noWrap
+              component="a"
+              href="/Services"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".1rem",
+                color: "White",
+                textDecoration: "none",
+                "&:hover": {
+                  color: "black !important",
+                  backgroundColor: "White",
+                },
+              }}
+            >
+              Services
+            </Button>
+            <Button
+              variant="h7"
+              noWrap
+              component="a"
+              href="/about"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".1rem",
+                color: "White",
+                textDecoration: "none",
+                "&:hover": {
+                  color: "black !important",
+                  backgroundColor: "White",
+                },
+              }}
+            >
+              About Us
+            </Button>
+            <Button
+              variant="h7"
+              noWrap
+              component="a"
+              href="/appointment"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".1rem",
+                color: "White",
+                textDecoration: "none",
+                "&:hover": {
+                  color: "black !important",
+                  backgroundColor: "White",
+                },
+              }}
+            >
+              Appointment
+            </Button>
+          </Box>
+*/
