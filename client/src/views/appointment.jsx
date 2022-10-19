4a79b0e0280e77793843dd5e7765e22da002a17d
Val2049 <valbakalor@gmail.com>
2022-10-19 11:12:28 -0700

    Changes

--- client/src/views/appointment.jsx
+++ client/src/views/appointment.jsx
@@ -51,125 +51,125 @@ export default function Appointment() {
       alignItems: "center",
     },
   };
-  return (
-    <div className="background-parent" style={styles.root}>
-      <mui.Box
-        component="div"
-        id="appointment-box-outer"
-        margin={"auto"}
-        overflow="revert"
-        sx={{
-          backdropFilter: "blur(10px)",
-          padding: "20px",
-          width: {md: "70%", sm: "100%", lg: "50%"},
-          height: {md: "80%", sm: "100%", lg: "50%"},
-          display: "inline-flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          marginBottom: "auto",
-          "&:hover": {
-            transition: "all 0.8s ease-in",
-            backdropFilter: "blur(70px) brightness(0.9)",
-            boxShadow: "0 0 20px 0 rgba(0, 0, 0, 0.5)",
-            borderRadius: "100px",
-            height: { xl: "45%", lg: "50%", md: "65%", sm: "70%", xs: "75%" },
-            width: { xl: "60%", lg: "65%", md: "85%", sm: "90", xs: "95%" },
-            marginBottom: "40px",
-          },
-        }}
-      >
-        <mui.Paper
-          id="appointment-Paper-inner"
-          component="div"
-          margin={"auto"}
-          overflow="revert"
-          sx={{
-            background: "transparent",
-          }}
-        >
-          <mui.Stack
-            id="appointment-stack"
-            direction="column"
-            spacing={15}
-            elevation={0}
-          >
-            <mui.Typography
-              variant="h3"
-              sx={{
-                position: "relative",
-                fontWeight: "bold",
-                fontSize: "3rem",
-                fontFamily: "Roboto",
-                textAlign: "center",
-                overflow: "hidden",
-                "@media (max-width: 1000px)": {
-                  fontSize: "2rem",
-                },
-              }}
+    let div = <>
+        <div className="background-parent" style={styles.root}>
+            <mui.Box
+                component="div"
+                id="appointment-box-outer"
+                margin={"auto"}
+                overflow="revert"
+                sx={{
+                    backdropFilter: "blur(10px)",
+                    padding: "20px",
+                    width: {md: "70%", sm: "100%", lg: "50%"},
+                    height: {md: "80%", sm: "100%", lg: "50%"},
+                    display: "inline-flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginBottom: "auto",
+                    "&:hover": {
+                        transition: "all 0.8s ease-in",
+                        backdropFilter: "blur(70px) brightness(0.9)",
+                        boxShadow: "0 0 20px 0 rgba(0, 0, 0, 0.5)",
+                        borderRadius: "100px",
+                        height: {xl: "45%", lg: "50%", md: "65%", sm: "70%", xs: "75%"},
+                        width: {xl: "60%", lg: "65%", md: "85%", sm: "90", xs: "95%"},
+                        marginBottom: "40px",
+                    },
+                }}
             >
-              Let's make an Appointment
-            </mui.Typography>
-            <mui.TextField
-              id="outlined-basic"
-              type="text"
-              label="Name"
-              variant="outlined"
-              placeholder="Enter your name"
-              required={true}
-              sx={{
-                width: "inherit",
-                height: "50px",
-                fontStyle: "italic",
-                textAlign: "center",
-                padding: "10px",
-                display: "flex",
-                "& label.Mui-focused": {
-                  color: "Black",
-                  textAlign: "center",
-                },
-                "& label.Mui": {
-                  textAlign: "center",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderColor: "black",
-                  },
-                  "& fieldset.Mui": {
-                    borderColor: "black",
-                  },
-                  "& fieldset.Mui-focused": {
-                    borderColor: "white",
-                  },
-                },
-              }}
-            />
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DatePicker
-                label="Pick Date"
-                value={date}
-                onChange={(newValue) => {
-                  setDate(newValue);
-                }}
-                renderInput={(params) => <mui.TextField {...params} />}
-              />
-              <TimePicker
-                renderInput={(params) => <mui.TextField {...params} />}
-                value={time}
-                label="Pick Time"
-                onChange={(newValue) => {
-                  setTime(newValue);
-                }}
-                minTime={dayjs("T06:00PM")}
-                maxTime={dayjs("T10:00AM")}
-              />
-            </LocalizationProvider>
-            <mui.Button variant="contained" onClick={onAppointment} sx={{}}>
-              Book Appointment
-            </mui.Button>
-          </mui.Stack>
-        </mui.Paper>
-      </mui.Box>
-    </div>
-  );
+                <mui.Paper
+                    id="appointment-Paper-inner"
+                    component="div"
+                    margin={"auto"}
+                    overflow="revert"
+                    sx={{
+                        background: "transparent",
+                    }}
+                >
+                    <mui.Stack
+                        id="appointment-stack"
+                        direction="column"
+                        spacing={15}
+                        elevation={0}
+                    >
+                        <mui.Typography
+                            variant="h3"
+                            sx={{
+                                position: "relative",
+                                fontWeight: "bold",
+                                fontSize: "3rem",
+                                fontFamily: "Roboto",
+                                textAlign: "center",
+                                overflow: "hidden",
+                                "@media (max-width: 1000px)": {
+                                    fontSize: "2rem",
+                                },
+                            }}
+                        >
+                            Let's make an Appointment
+                        </mui.Typography>
+                        <mui.TextField
+                            id="outlined-basic"
+                            type="text"
+                            label="Name"
+                            variant="outlined"
+                            placeholder="Enter your name"
+                            required={true}
+                            sx={{
+                                width: "inherit",
+                                height: "50px",
+                                fontStyle: "italic",
+                                textAlign: "center",
+                                padding: "10px",
+                                display: "flex",
+                                "& label.Mui-focused": {
+                                    color: "Black",
+                                    textAlign: "center",
+                                },
+                                "& label.Mui": {
+                                    textAlign: "center",
+                                },
+                                "& .MuiOutlinedInput-root": {
+                                    "& fieldset": {
+                                        borderColor: "black",
+                                    },
+                                    "& fieldset.Mui": {
+                                        borderColor: "black",
+                                    },
+                                    "& fieldset.Mui-focused": {
+                                        borderColor: "white",
+                                    },
+                                },
+                            }}
+                        />
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker
+                                label="Pick Date"
+                                value={date}
+                                onChange={(newValue) => {
+                                    setDate(newValue);
+                                }}
+                                renderInput={(params) => <mui.TextField {...params} />}
+                            />
+                            <TimePicker
+                                renderInput={(params) => <mui.TextField {...params} />}
+                                value={time}
+                                label="Pick Time"
+                                onChange={(newValue) => {
+                                    setTime(newValue);
+                                }}
+                                minTime={dayjs("T06:00PM")}
+                                maxTime={dayjs("T10:00AM")}
+                            />
+                        </LocalizationProvider>
+                        <mui.Button variant="contained" onClick={onAppointment} sx={{}}>
+                            Book Appointment
+                        </mui.Button>
+                    </mui.Stack>
+                </mui.Paper>
+            </mui.Box>
+        </div>
+    </>;
 }
