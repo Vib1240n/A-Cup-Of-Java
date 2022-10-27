37692abc8935e6b92f046381aebb525bf7fd5536
Vib1240n <vibhorsgr@gmail.com>
2022-10-27 16:21:25 -0700

    commit

--- client/src/views/home.jsx
+++ client/src/views/home.jsx
@@ -1,162 +1,178 @@
 import React from "react";
-import { useRef } from "react";
+import {useRef} from "react";
 import * as m from "@mui/material";
 //import "../asset/css/style.css";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import Background from "../asset/images/BarbershopBackground.png";
 import servicesbg from "../asset/images/servicesbackground.jpg";
+import wallBg from "../asset/images/wall-bg-up.jpg";
 
 function Home() {
-  const [open, setOpen] = React.useState(false);
-  const ref = useRef(null);
-  let navigate = useNavigate();
+    const [open, setOpen] = React.useState(false);
+    const ref = useRef(null);
+    let navigate = useNavigate();
 
-  const handleChange = () => {
-    navigate("/appointment");
-  };
+    const handleChange = () => {
+        navigate("/appointment");
+    };
 
-  const styles = {
-    background: {
-      background: `url(${Background})`,
-      position: "fixed",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      width: "100vw",
-      zIndex: "-1",
-    },
-    listItem: {
-      display: "flex",
-      backgroundColor: "transparent",
-      backdropFilter: "blur(20px) brightness(0.2)",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
-      height: "100vh",
-      width: "100vw",
-      top: "0",
-    },
-    listItemBox: {
-      background: "rgba(255, 255, 255, 0.1)",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "row",
-      height: "100vh",
-      width: "100vw",
-      top: "0",
-      marginBottom: "0",
-    },
-    servicesBackground: {
-      background: `url(${servicesbg})`,
-      borderRadius: "20px",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      backgroundPosition: "top",
-      display: "flex",
-      flexDirection: "column",
-      height: "100vh",
-      width: "100vw",
-      fontSize: "2rem",
-      color: "white",
-    },
-    servicesBackgroundbox: {
-      backgroundColor: "transparent",
-      backdropFilter: "blur(70px)",
-      border: "1px solid white",
-      display: "flex",
-      justifyContent: "flex-start",
-      alignItems: "center",
-      flexDirection: "row",
-      height: "75%",
-      width: "100%",
-      borderRadius: "10px",
-    },
-    typography: {
-      fontFamily: "Roboto",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
-      marginTop: "55px",
-      margin: "auto",
-      fontSize: {
-        xl: "5rem",
-        lg: "3rem",
-        md: "2rem",
-        sm: "1.5rem",
-        xs: "1.5rem",
-      },
-      color: "white",
-    },
-    gridLayout: {
-      display: "flex",
-      justifyContent: "flex-start",
-      alignItems: "center",
-      flexDirection: "column",
-      height: "90%",
-      width: "100%",
-    },
-  };
+    const styles = {
+        background: {
+            background: `url(${Background})`,
+            position: "fixed",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+            zIndex: "-1",
+        },
+        listItem: {
+            display: "flex",
+            backgroundColor: "transparent",
+            backdropFilter: "blur(20px) brightness(0.2)",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            height: "100vh",
+            width: "100vw",
+            top: "0",
+        },
+        listItemBox: {
+            background: "rgba(255, 255, 255, 0.1)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "row",
+            height: "100vh",
+            width: "100vw",
+            top: "0",
+            marginBottom: "0",
+        },
+        servicesBackground: {
+            background: `url(${servicesbg})`,
+            borderRadius: "20px",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "top",
+            display: "flex",
+            flexDirection: "column",
+            height: "100vh",
+            width: "100vw",
+            fontSize: "2rem",
+            color: "white",
+        },
+        servicesBackgroundbox: {
+            backgroundColor: "transparent",
+            backdropFilter: "blur(70px)",
+            border: "1px solid white",
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            flexDirection: "row",
+            height: "75%",
+            width: "100%",
+            borderRadius: "10px",
+        },
+        typography: {
+            fontFamily: "Roboto",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            marginTop: "55px",
+            margin: "auto",
+            fontSize: {
+                xl: "5rem",
+                lg: "3rem",
+                md: "2rem",
+                sm: "1.5rem",
+                xs: "1.5rem",
+            },
+            color: "white",
+        },
+        gridLayout: {
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            flexDirection: "column",
+            height: "90%",
+            width: "100%",
+        },
+    };
 
-  const list = () => (
-    <m.List>
-      <m.ListItem id="home" style={styles.listItem} className="listItem">
-        <m.Box style={styles.listItemBox} className="listItemBox">
-            <m.Typography style={styles.typography} variant="h2" className="Typography">
-            Welcome to Ace's Barbershop
-          </m.Typography>
-        </m.Box>
-      </m.ListItem>
-      <m.ListItem
-        id="services-view"
-        style={styles.listItem}
-        className="listItem"
-      >
-        <m.Box style={styles.servicesBackground} className="servicesBackground">
-          <m.Typography style={styles.typography} variant="h3">Services</m.Typography>
-          <m.Box
-            style={styles.servicesBackgroundbox}
-            className="servicesBackgroundbox"
-          >
-            <m.Box
-              sx={{
+    const contactInfo = () => {
+        return (<m.Box
+            sx={{
+                backgroundImage: `url(${wallBg})`,
+                backgroundPosition: "center",
+                WebkitFilter: "blur(10px)",
                 display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-                height: "60%",
-                width: "50%",
-                margin: "auto",
-                border: "1px solid white",
-                backgroundColor: "rgba(255, 0, 0)",
-              }}
-            ></m.Box>
-          </m.Box>
-        </m.Box>
-      </m.ListItem>
-      <m.ListItem ref={ref} id="barber-information" style={styles.listItem}>
-        <m.Box style={styles.listItemBox}>Barbers</m.Box>
-      </m.ListItem>
-      <m.ListItem ref={ref} id="contact-infomration" style={styles.listItem}>
-        <m.Box style={styles.listItemBox}>Contact Us</m.Box>
-      </m.ListItem>
-      <m.ListItem id="about-us" style={styles.listItem}>
-        <m.Box style={styles.listItemBox}>About Us</m.Box>
-      </m.ListItem>
-    </m.List>
-  );
+                width: "95%",
+                height: "95%",
+            }}
+        >
 
-  return (
-    <div id="base-page">
-      <img style={styles.background} id="base-page"></img>
-      <m.Box>{list()}</m.Box>
-    </div>
-  );
+        </m.Box>);
+    };
+
+    const list = () => (
+        <m.List>
+            <m.ListItem id="home" style={styles.listItem} className="listItem">
+                <m.Box style={styles.listItemBox} className="listItemBox">
+                    <m.Typography style={styles.typography} variant="h2" className="Typography">
+                        Welcome to Ace's Barbershop
+                    </m.Typography>
+                </m.Box>
+            </m.ListItem>
+            <m.ListItem
+                id="services-view"
+                style={styles.listItem}
+                className="listItem"
+            >
+                <m.Box style={styles.servicesBackground} className="servicesBackground">
+                    <m.Typography style={styles.typography} variant="h3">Services</m.Typography>
+                    <m.Box
+                        style={styles.servicesBackgroundbox}
+                        className="servicesBackgroundbox"
+                    >
+                        <m.Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                flexDirection: "column",
+                                height: "60%",
+                                width: "50%",
+                                margin: "auto",
+                                border: "1px solid white",
+                                backgroundColor: "rgba(255, 0, 0)",
+                            }}
+                        ></m.Box>
+                    </m.Box>
+                </m.Box>
+            </m.ListItem>
+            <m.ListItem ref={ref} id="barber-information" style={styles.listItem}>
+                <m.Box style={styles.listItemBox}>Barbers</m.Box>
+            </m.ListItem>
+            <m.ListItem ref={ref} id="contact-infomration" style={styles.listItem}>
+                <m.Box style={styles.listItemBox}>{contactInfo()}</m.Box>
+            </m.ListItem>
+            <m.ListItem id="about-us" style={styles.listItem}>
+                <m.Box style={styles.listItemBox}>About Us</m.Box>
+            </m.ListItem>
+        </m.List>
+    );
+
+    return (
+        <div id="base-page">
+            <img style={styles.background} id="base-page"></img>
+            <m.Box>{list()}</m.Box>
+        </div>
+    );
 }
 
 export default Home;
--- client/src/views/signUpPage.jsx
+++ client/src/views/signUpPage.jsx
@@ -3,223 +3,273 @@ import * as m from "@mui/material";
 import axios from "axios";
 import background from "../asset/images/background_cornerLong.jpg";
 import Alert from "@mui/material/Alert";
+import valid from 'validator';
+
 
 export default function loginPage() {
-  React.useEffect(() => {
-    isLoggedIn();
-  }, []);
-  /* 
-  On change events for the login page & signup page
-  */
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [firstName, setFirstName] = React.useState("");
-  const [lastName, setLastName] = React.useState("");
-  const [phoneNumber, setPhoneNumber] = React.useState("");
-  const [confirmPassword, setConfirmPassword] = React.useState("");
+    React.useEffect(() => {
+        isLoggedIn();
+    }, []);
+    /*
+    On change events for the login page & signup page
+    */
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [firstName, setFirstName] = React.useState("");
+    const [lastName, setLastName] = React.useState("");
+    const [phoneNumber, setPhoneNumber] = React.useState("");
+    const [confirmPassword, setConfirmPassword] = React.useState("");
+    const [errorMessage, setErrormessage] = React.useState(false);
+    const [errorEmail, setErrorEmail] = React.useState(false);
 
-  /* default styles */
-  const styles = {
-    root: {
-      backgroundImage: `url(${background})`,
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backdropFilter: "blur(10px)",
-      height: "100vh",
-      width: "100vw",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    TextField: {
-      color: "white",
-      height: "10%",
-      width: { xl: "40%" },
-      fontSize: "1.5rem",
-      borderRadius: "10px",
-      paddingLeft: "3px",
-      paddingRight: "4px",
-      paddingBottom: "10px",
-      "& .MuiInputBase-input": {
-        color: "white",
-        textAlign: "center",
-      },
-      "& .MuiInputLabel-root": {
-        color: "white",
-        fontSize: "1.5rem",
-      },
-      "& .MuiInputLabel-root.Mui-focused": {
-        color: "green",
-        textAlign: "center",
-        fontSize: "1.2rem",
-      },
-      "& .MuiInput-underline:before": {
-        borderBottom: "2px solid white",
-      },
-      "& .MuiInput-underline:after": {
-        borderBottom: "2px solid black",
-      },
-      "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
-        borderBottom: "2px solid green",
-      },
-    },
-    inputProps: {
-      style: {
-        color: "white",
-        fontSize: "1.5rem",
-        borderRadius: "10px",
-        paddingLeft: "0px",
-      },
-    },
-  };
+    /* Error message*/
+    const errorBox = errorMessage ? <Alert severity="error" variant="filled">Please check passwords</Alert> : <Alert severity="success">Signed Up!</Alert>;
 
-  const isLoggedIn = () => {
-    axios
-      .get("http://localhost:5500/api/profile")
-      .then((res) => {
-        console.log("Is user logged in status: " + res.status);
-        if (res.status === 200) {
-          window.Location = "/MyProfile";
+    const errorEmailBox = errorEmail ? <Alert severity="error" variant="filled">Email is incorrect</Alert> : "";
+
+    const isValidEmail = (_prop) => {
+        console.log(_prop);
+        _prop=_prop.toString();
+        console.log(_prop);
+        if (!_prop) {
+            setErrorEmail(true);
+        } else if (valid.isEmail(_prop)) {
+            setEmail(_prop);
+        } else {
+            setErrorEmail(true);
         }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    };
 
-  const logPage = () => {
-    return (
-      <m.Stack
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={5}
-        height="100%"
-        width="100%"
-      >
-        <m.TextField
-          id="loginpage-username"
-          label="First Name"
-          type={"text"}
-          variant="standard"
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-          notched
-          disableUnderline={true}
-          sx={styles.TextField}
-          inputProps={styles.inputProps}
-        ></m.TextField>
-        <m.TextField
-          id="loginpage-username"
-          label="Last Name"
-          type={"text"}
-          variant="standard"
-          onChange={(e) => setLastName(e.target.value)}
-          required
-          sx={styles.TextField}
-          inputProps={styles.inputProps}
-        ></m.TextField>
-        <m.TextField
-          id="loginpage-username"
-          label="Password"
-          type={"password"}
-          variant="standard"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          sx={styles.TextField}
-          inputProps={styles.inputProps}
-        ></m.TextField>
-        <m.TextField
-          id="loginpage-username"
-          label="Confirm Password"
-          type={"password"}
-          variant="standard"
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-          sx={styles.TextField}
-          inputProps={styles.inputProps}
-        ></m.TextField>
-        <m.Button
-          id="loginpage-button"
-          onClick={onSubmitSignUp}
-          sx={{
+    /* default styles */
+    const styles = {
+        root: {
+            backgroundImage: `url(${background})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backdropFilter: "blur(10px)",
+            height: "100vh",
+            width: "100vw",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        TextField: {
+            color: "white",
             height: "10%",
-            width: { xl: "40%" },
-
+            width: {xl: "40%"},
+            fontSize: "1.5rem",
             borderRadius: "10px",
-            border: "2px solid white",
             paddingLeft: "3px",
             paddingRight: "4px",
-            color: "black",
-            "&:hover": {
-              boxShadow: "white 0px 0px 5px 0.1rem",
-              backgroundColor: "rgba(255, 255, 255, 0.7)",
+            paddingBottom: "10px",
+            "& .MuiInputBase-input": {
+                color: "white",
+                textAlign: "center",
             },
-          }}
-        >
-          <m.Typography
-            sx={{
-              fontSize: "1.5rem",
-              height: "auto",
-              width: "200%",
-              justifyContent: "center",
-              alignItems: "center",
-              "&:hover": {
-                transform: "translateX(-20%)",
-                transition: "all 0.9s ease-in",
-              },
-            }}
-          >
-            Sign Up
-          </m.Typography>
-        </m.Button>
-      </m.Stack>
-    );
-  };
-
-  /*  
-  on Submit function for the login page  
-  */
-  const onSubmitSignUp = (e) => {
-    e.preventDefault();
-    const user = {
-      username: email,
-      firstName: firstName,
-      lastName: lastName,
-      password: password,
+            "& .MuiInputLabel-root": {
+                color: "white",
+                fontSize: "1.5rem",
+            },
+            "& .MuiInputLabel-root.Mui-focused": {
+                color: "green",
+                textAlign: "center",
+                fontSize: "1.2rem",
+            },
+            "& .MuiInput-underline:before": {
+                borderBottom: "2px solid white",
+            },
+            "& .MuiInput-underline:after": {
+                borderBottom: "2px solid black",
+            },
+            "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+                borderBottom: "2px solid green",
+            },
+        },
+        inputProps: {
+            style: {
+                color: "white",
+                fontSize: "1.5rem",
+                borderRadius: "10px",
+                paddingLeft: "0px",
+            },
+        },
     };
-    axios
-      .post("http://localhost:5500/api/signup", user)
-      .then((res) => {
-        console.log(res.data);
-        if (res.data.redirect === "/MyProfile") {
-          window.location = "/MyProfile";
+
+    const isLoggedIn = () => {
+        axios
+            .get("http://localhost:5500/api/profile")
+            .then((res) => {
+                console.log("Is user logged in status: " + res.status);
+                if (res.status === 200) {
+                    window.Location = "/MyProfile";
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    const logPage = () => {
+        return (
+            <m.Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={7}
+                height="100%"
+                width="100%"
+            >
+                <m.TextField
+                    id="loginpage-username"
+                    label="First Name"
+                    type={"text"}
+                    variant="standard"
+                    onChange={(e) => setFirstName(e.target.value)}
+                    required
+                    notched
+                    disableUnderline={true}
+                    sx={styles.TextField}
+                    inputProps={styles.inputProps}
+                ></m.TextField>
+                <m.TextField
+                    id="loginpage-username"
+                    label="Last Name"
+                    type={"text"}
+                    variant="standard"
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                    sx={styles.TextField}
+                    inputProps={styles.inputProps}
+                ></m.TextField>
+                <m.TextField
+                    id="loginpage-username"
+                    label="Email"
+                    type={"email"}
+                    variant="standard"
+                    onChange={(e) => setEmail(e.target.value.toString())}
+                    required
+                    sx={styles.TextField}
+                    inputProps={styles.inputProps}
+                ></m.TextField>
+                <m.TextField
+                    id="loginpage-username"
+                    label="Phone Number"
+                    type={"number"}
+                    variant="standard"
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    required
+                    sx={styles.TextField}
+                    inputProps={styles.inputProps}
+                ></m.TextField>
+                <m.TextField
+                    id="loginpage-username"
+                    label="Password"
+                    type={"password"}
+                    variant="standard"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    sx={styles.TextField}
+                    inputProps={styles.inputProps}
+                ></m.TextField>
+                <m.TextField
+                    id="loginpage-username"
+                    label="Confirm Password"
+                    type={"password"}
+                    variant="standard"
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                    sx={styles.TextField}
+                    inputProps={styles.inputProps}
+                ></m.TextField>
+
+                <m.Button
+                    id="loginpage-button"
+                    onClick={onSubmitSignUp && isValidEmail}
+                    sx={{
+                        height: "10%",
+                        width: {xl: "40%"},
+
+                        borderRadius: "10px",
+                        border: "2px solid white",
+                        paddingLeft: "3px",
+                        paddingRight: "4px",
+                        color: "black",
+                        "&:hover": {
+                            boxShadow: "white 0px 0px 5px 0.1rem",
+                            backgroundColor: "rgba(255, 255, 255, 0.7)",
+                        },
+                    }}
+                >
+                    <m.Typography
+                        sx={{
+                            fontSize: "1.5rem",
+                            height: "auto",
+                            width: "200%",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            "&:hover": {
+                                transform: "translateX(-20%)",
+                                transition: "all 0.9s ease-in",
+                            },
+                        }}
+                    >
+                        Sign Up
+                    </m.Typography>
+                </m.Button>
+                {errorEmailBox}
+                {errorBox}
+            </m.Stack>
+        );
+    };
+
+    /*
+    on Submit function for the login page
+    */
+    const onSubmitSignUp = (e) => {
+        if (password === confirmPassword) {
+            e.preventDefault();
+            const user = {
+                username: email.toString().toLowerCase(),
+                firstName: firstName,
+                lastName: lastName,
+                password: password,
+                phoneNumber: phoneNumber,
+            };
+            axios
+                .post("http://localhost:5500/api/signup", user)
+                .then((res) => {
+                    console.log(res.data);
+                    if (res.data.redirect === "/MyProfile") {
+                        window.location = "/MyProfile";
+                    }
+                })
+                .catch((err) => console.log(err));
+        } else {
+            setErrormessage(true);
         }
-      })
-      .catch((err) => console.log(err));
-  };
-  /*
-  Return for the login page
-  */
-  return (
-    <div className="loginpage-basecontainer" style={styles.root}>
-      <m.Box
-        component="div"
-        id="loginpage-box-outer"
-        margin={"auto"}
-        overflow="revert"
-        sx={{
-          height: { md: "80%", sm: "90%", lg: "70%", xs: "75%" },
-          width: { md: "70%", sm: "100%", lg: "50%", xs: "100%" },
-          backdropFilter: "blur(50px)",
-          boxShadow:
-            "0 9px 10px 0px gray, 0 -9px 10px 0px rgba(255, 255, 255, 0.8), 12px 0 15px -4px rgba(255, 0, 0, 1), -12px 0 15px -4px rgba(3, 3, 3, 0.5)",
-          borderRadius: "10px",
-        }}
-      >
-        <m.Stack>{logPage()}</m.Stack>
-      </m.Box>
-    </div>
-  );
+    };
+    /*
+    Return for the login page
+    */
+    return (
+        <div className="loginpage-basecontainer" style={styles.root}>
+            <m.Box
+                component="div"
+                id="loginpage-box-outer"
+                margin={"auto"}
+                overflow="revert"
+                sx={{
+                    height: {md: "80%", sm: "90%", lg: "70%", xs: "75%"},
+                    width: {md: "70%", sm: "100%", lg: "50%", xs: "100%"},
+                    backdropFilter: "blur(50px)",
+                    boxShadow:
+                        "0 9px 10px 0px gray, 0 -9px 10px 0px rgba(255, 255, 255, 0.8), 12px 0 15px -4px rgba(255, 0, 0, 1), -12px 0 15px -4px rgba(3, 3, 3, 0.5)",
+                    borderRadius: "10px",
+                }}
+            >
+                <m.Stack>{logPage()}</m.Stack>
+            </m.Box>
+        </div>
+    );
 }
